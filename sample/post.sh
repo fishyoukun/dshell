f31e88523308938567f5a3590344485b9bf2c3b9
Kun.You <fishyoukun@gmail.com>
2020-02-15 18:27:26 +0800

    enable makefile

---
+++ sample/post.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo -n "get origin file length"
+binsize=`du -b $1 |cut -f1 `
+echo -n "...bin_size $binsize..." 
+echo "ok"
+echo -n "append symbol..."
+cat $2 >>$1
+echo "ok"
+echo -n "append mark..."
+echo "KISS">>$1
+echo "ok"
+echo -n "append origin length..."
+echo $binsize>>$1
+echo "ok"
+echo -n "copy a.out..."
+cp $1 ../utils/
+echo "ok"
